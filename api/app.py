ff87023cab17b3cae406e958ca388927fc7f2421
Hiromi-55 <h.nagabayashi@svd.co.jp>
2025-08-07 17:45:00 +0900

    Create app.py

---
+++ api/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api/app", methods=["POST"])
+def handle_request():
+    data = request.get_json()
+    target_url = data.get("target_url")
+    management_number = "仮の管理番号"
+    return jsonify({"management_number": management_number})
